f94dffbbdaeb678315bb3924203d6afeed717bb0
薛建恒 <CORELINK\jianheng.xue@hpc.corelink.vip>
2020-01-10 14:36:53 +0800

    cuda event recore

--- F4/Testdata/count.sh
+++ F4/Testdata/count.sh
@@ -9,7 +9,7 @@ do
 	grep 'Total Time:' output_FGMRES_ILU_0.dat| tee -a ../count_Total_Time.dat
 	grep 'computeLUFactors Time:'  output_FGMRES_ILU_0.dat| tee -a ../count_computeLUfactors.dat
 	grep '    solve: '   output_FGMRES_ILU_0.dat| tee -a ../count_FGMRES.dat
-	grep 'Smoother Time:' output_FGMRES_ILU_0.dat  | tail -1 | tee -a ../count_Smoother.dat
-        grep 'Smoother Time:' output_FGMRES_ILU_0.dat  -c | tee -a ../count_number.dat
+	grep 'Smoother used ' output_FGMRES_ILU_0.dat  | tail -1 | tee -a ../count_Smoother.dat
+        grep 'Smoother used ' output_FGMRES_ILU_0.dat  -c | tee -a ../count_number.dat
  	cd ../
 done
